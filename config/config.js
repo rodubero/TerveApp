905ed70ff98affebd88a408ffd8d4af55bb0a0fc
Ron <35814029+rodubero@users.noreply.github.com>
2020-12-14 23:18:30 +0200

    Heroku added

--- config/config.js
+++ config/config.js
@@ -1,8 +1,6 @@
 let config = {};
 
 if (Deno.env.get('TEST_ENVIRONMENT')) {
-  config.database = {};
-} else {
   config.database = {
     hostname: "hattie.db.elephantsql.com",
     database: "hawrridp",
@@ -10,6 +8,22 @@ if (Deno.env.get('TEST_ENVIRONMENT')) {
     password: "ZlDLkkNtdrLsjLB5HzGjm3ORyz8WndoR",
     port: 5432
   };
+} else {
+  config.database = {
+    hostname: Deno.env.toObject().HOSTNAME,
+    database: Deno.env.toObject().DATABASE,
+    user: Deno.env.toObject().USER,
+    password: Deno.env.toObject().PASSWORD,
+    port: Deno.env.toObject().PORT
+  };
 }
+
+/*
+    hostname: "hattie.db.elephantsql.com",
+    database: "hawrridp",
+    user: "hawrridp",
+    password: "ZlDLkkNtdrLsjLB5HzGjm3ORyz8WndoR",
+    port: 5432
+*/
 
 export { config }; 
