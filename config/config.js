3b1b430f78769719644cc40374dc55dab11f71cf
Ron <35814029+rodubero@users.noreply.github.com>
2020-12-15 09:02:29 +0200

    DB Credentials updated

--- config/config.js
+++ config/config.js
@@ -4,13 +4,7 @@ if (Deno.env.get('DATABASE_URL')) {
   config.database = Deno.env.get('DATABASE_URL');
 } else {
   config.database = {
-    //Credentials still here because I could not set it up properly for environmental variables in windows environment
 
-    hostname: "hattie.db.elephantsql.com",
-    database: "hawrridp",
-    user: "hawrridp",
-    password: "ZlDLkkNtdrLsjLB5HzGjm3ORyz8WndoR",
-    port: 5432
 
   };
 }
